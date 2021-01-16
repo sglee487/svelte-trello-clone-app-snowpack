63b80cd15f3b25322ebd6f43fcb4d7fb1b9685ff
isang-geon macair <sglee487@gmail.com>
2021-01-16 20:21:45 +0900

    Create project

---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<header>
+    <img src="/images/trello-logo.svg"
+    alt="Trello" 
+    class='logo' />
+</header>
+
+<style lang="scss">
+    header {
+        height: 40px;
+        box-sizing: border-box;
+        background-color: rgba(black,.4);
+        display: flex;
+        // display: -ms-flexbox;
+        // display: -webkit-flex;
+        // display: -moz-box;
+        justify-content: center;
+        align-items: center;
+        img.logo {
+            width: 80px;
+            opacity: .5;
+        }
+    }
+</style>
